7ee2bbe539c81133937739561d806d1dd23848dd
Trent Rivers <trentbrivers@gmail.com>
2025-04-15 00:13:20 -0500

    more tests

--- test_scripts/pypdf_parse_text.py
+++ test_scripts/pypdf_parse_text.py
@@ -1,5 +1,10 @@
 from pypdf import PdfReader
-
+"""
 reader = PdfReader("TrentRiversResume.pdf")
+for page in reader.pages:
+	print(page.extract_text())
+"""
+
+reader = PdfReader("9a1e0e4d-7f23-4afd-9698-0ab93c74d4e4.pdf")
 for page in reader.pages:
 	print(page.extract_text())
--- test_scripts/tika_pdf_parse_test.py
+++ test_scripts/tika_pdf_parse_test.py
@@ -1,4 +1,7 @@
 from tika import parser
 
-text = parser.from_file("TrentRiversResume.pdf")
+#text = parser.from_file("TrentRiversResume.pdf")
+#print(text['content'])
+
+text = parser.from_file("9a1e0e4d-7f23-4afd-9698-0ab93c74d4e4.pdf")
 print(text['content'])
